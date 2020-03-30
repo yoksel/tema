21f533a7bb7e5d727dd8da263dc6179e1801e0eb
yoksel <yokselzok@gmail.com>
2020-03-30 08:11:13 +0300

    Clear gitignore; fix deploy

--- prod.config.js
+++ prod.config.js
@@ -10,7 +10,7 @@ module.exports = () => {
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/grid-cheatsheet/'
+      publicPath: '/tema/'
     },
     module: {
       rules: [
